c7686621126693aad6b0b11f7dc3d7312d8ad073
Jay Kumrawat <kumrawatjay07@gmail.com>
2026-05-09 20:41:14 +0530

    updated cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -10,37 +9,37 @@ const app = express();
 app.use(helmet());
 
 //cors - allow react frontend to call this API
-app.use(cors({
-    origin: "http://localhost:5173", // React dev server
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://frontend-deviq.vercel.app/", // React dev server
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 //body parsers
-app.use(express.json({limit: '10kb'}));
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
 
 //request logger (dev only)
-if(process.env.NODE_ENV === 'developement'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "developement") {
+  app.use(morgan("dev"));
 }
 
-const authRoutes = require('./routes/auth.routes');
-const githubRoutes = require('./routes/github.routes');
-const historyRoutes = require('./routes/history.routes');
-const compareRoutes = require('./routes/compare.routes');
-const reportRoutes = require('./routes/report.routes')
-const errorHandler = require('./middleware/errorHandler');
+const authRoutes = require("./routes/auth.routes");
+const githubRoutes = require("./routes/github.routes");
+const historyRoutes = require("./routes/history.routes");
+const compareRoutes = require("./routes/compare.routes");
+const reportRoutes = require("./routes/report.routes");
+const errorHandler = require("./middleware/errorHandler");
 
 //routes
-app.use('/api/auth', authRoutes);
-app.use('/api/github', githubRoutes);
-app.use('/api/history', historyRoutes);
-app.use('/api/compare', compareRoutes);
-app.use('/api/reports', reportRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/github", githubRoutes);
+app.use("/api/history", historyRoutes);
+app.use("/api/compare", compareRoutes);
+app.use("/api/reports", reportRoutes);
 
 app.use(errorHandler);
 
 module.exports = app;
-
